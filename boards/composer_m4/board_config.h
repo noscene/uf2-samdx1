dd773bc013ca96aa84abd615922b1c3caf4fdbfa
Sven Braun <me@no-scene.de>
2019-05-02 17:02:44 +0200

    add composer

---
+++ boards/composer_m4/board_config.h
@@ -0,0 +1,47 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+
+/*          DOPPLER-Board-Layout:
+ *                                                                                    ---------------- FPGA Pins ------------------
+ *                                            RX       DAC1  TX            DAC0      LedR LedG LedB
+ * DIL Pin 48   47   46   45   44   43   42   41   40   39   38   37   36   35   34   33   32   31   30   29   28   27   26   25
+ *       |--O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O---|
+ * name  | VIN  5V  3.3V  A10  A9   A8   A7   A6   A5   A4   A3   A2   A1   A0   GND  R2   R1   R0   F14  F13  F12  F11  F10  F9   |
+ * alt   | VIN  5V  3.3V PA11 PA10 PA09 PA08 PA07 PA06 PA05 PA04 PB09 PB08 PA02  GND  41   40   39   38   37   36   35   34   32   |
+ *       |                                                                                            ö  ö  ö  ö                   |
+ *      |                                                                                             ö  ö  ö  ö         |BTN:S1|  |
+ *     | USB                           DOPPLER: SamD51 <- SPI -> icE40        |BTN:RESET|             ö  ö  ö  ö                   |
+ *      |                                                                                             ö  ö  ö  ö         |BTN:S2|  |
+ *       |                                                                                                                         |
+ * alt   | GND PA13 PA12 PB11 PA14 PA15 PB10 PA31 PA30  RES PA19 PA20 PA21 PA22 3.3V  11   12   13   18   19   20   21   23   25   |
+ * name  | GND   0    1    2    3    4    5                   6    7    8    9  3.3V  F0   F1   F2   F3   F4   F5   F6   F7   F8   |
+ *       L--O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O---|
+ * DIL Pin  1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24
+ *             SCL  SDA                       SWD  SWC RES
+ *             -- I2C--                       --- SWD  ---   ----- Shared  -----      ---------------- FPGA Pins ------------------
+ */
+
+#define VENDOR_NAME "dadamachines"
+#define PRODUCT_NAME "composer"
+#define VOLUME_LABEL "COMPOSER"
+#define INDEX_URL "https://github.com/dadamachines/composer/"
+#define BOARD_ID "SAMD51J19A-composer-v0"
+
+#define USB_VID 0x16D0
+#define USB_PID 0x0E3B
+
+#define LED_PIN PIN_PB17
+
+#define BOOT_USART_MODULE                 SERCOM0
+#define BOOT_USART_MASK                   APBAMASK
+#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBAMASK_SERCOM0
+#define BOOT_USART_PAD_SETTINGS           UART_RX_PAD3_TX_PAD0
+#define BOOT_USART_PAD3                   PINMUX_PA07D_SERCOM0_PAD3
+#define BOOT_USART_PAD2                   PINMUX_UNUSED
+#define BOOT_USART_PAD1                   PINMUX_UNUSED
+#define BOOT_USART_PAD0                   PINMUX_PA04D_SERCOM0_PAD0
+#define BOOT_GCLK_ID_CORE                 SERCOM0_GCLK_ID_CORE
+#define BOOT_GCLK_ID_SLOW                 SERCOM0_GCLK_ID_SLOW
+
+#endif
